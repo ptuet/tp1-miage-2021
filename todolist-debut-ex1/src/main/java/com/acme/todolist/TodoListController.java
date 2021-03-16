f9f5854372ce07e757f83eae1bd906c6da152547
ptuet <47171444+ptuet@users.noreply.github.com>
2021-03-16 13:11:07 +0100

    Update TodoListController.java
    Ajout code exo 1

--- todolist-debut-ex1/src/main/java/com/acme/todolist/TodoListController.java
+++ todolist-debut-ex1/src/main/java/com/acme/todolist/TodoListController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.inject.Inject;
 /**
  * Le controlleur Spring MVC qui expose les endpoints REST
  * 
@@ -24,6 +25,7 @@ public class TodoListController {
 	private static final String LATE = "[LATE!]";
 	private TodoItemRepository todoItemRepository;
 
+	@Inject
 	public TodoListController(TodoItemRepository todoItemRepository) {
 		super();
 		this.todoItemRepository = todoItemRepository;
@@ -36,8 +38,7 @@ public class TodoListController {
 	@PostMapping("/todos")
 	@ResponseStatus(code = HttpStatus.CREATED)
 	public void createTodoItem(@RequestBody TodoItem todoItem) {
-		// Code à compléter
-		// ...
+		this.todoItemRepository.save(todoItem);
 	}
 
 	@GetMapping("/todos")
